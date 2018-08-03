bdd4926602536b3b47bd6ec605f3132026f087d5
ivkrasavchik <ivkrasavchik@gmail.com>
2018-08-03 14:26:13 +0300

    com

--- acc/views.py
+++ acc/views.py
@@ -3,21 +3,16 @@
 from django.shortcuts import render, render_to_response, redirect
 from django.contrib import auth
 from django.template.context_processors import csrf
-from django.views.decorators.csrf import csrf_protect
-from django.template import RequestContext
 
 
 def home(request):
     user = auth.get_user(request)
-    print("UUUUUUU", user.is_active, user.id, user.username)
     return render_to_response('home.html', {'user': auth.get_user(request)})
-    #return render(request, 'home.html', locals())
 
 
 # @csrf_protect
 def login(request):
     args = {}
-    # print("EEEEEEEE", csrf(request))
     args.update(csrf(request))
     if request.POST:
         username = request.POST.get('username', '')
@@ -34,7 +29,6 @@ def login(request):
 
     else:
         return render_to_response('login.html', args)
-        # return render(request, 'home.html', args)
 
 
 
